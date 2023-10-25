a3365d4501fe62902e4bf6a697df98d01e096f40
Damian Lobos <damianlobos24@gmail.com>
2023-10-25 13:02:29 -0300

    Player movement. doesn't work yet

---
+++ FloorIsLava/Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    private CharacterController controller;
+    [SerializeField]
+    private Transform cam;
+
+    private float speed = 6f;
+    private float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        Vector3 direction = new Vector3(-vertical, 0f, -horizontal).normalized;
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg * cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir * speed * Time.deltaTime);
+        }
+    }
+}
