5e7e4b75fae816249233da98e30310f8ba95f368
Damian Lobos <damianlobos24@gmail.com>
2023-11-24 13:11:14 -0300

    Update por fixedUpdate

--- FloorIsLava/Assets/Scripts/RigidBodyMovement.cs
+++ FloorIsLava/Assets/Scripts/RigidBodyMovement.cs
@@ -43,7 +43,7 @@ public class RigidBodyMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (IsMovementAllowed)
         {
