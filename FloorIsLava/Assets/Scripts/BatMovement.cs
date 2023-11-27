9c1943795b88b872effdf2091983f50890ed82c5
Damian Lobos <damianlobos24@gmail.com>
2023-11-27 11:40:23 -0300

    Mod de variable speed

--- FloorIsLava/Assets/Scripts/BatMovement.cs
+++ FloorIsLava/Assets/Scripts/BatMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BatMovement : MonoBehaviour
 {
     [SerializeField]
-    public float speed = -2f;
+    private float speed = -2f;
 
     // Update is called once per frame
     void Update()
